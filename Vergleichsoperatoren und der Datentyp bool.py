75f70eeafba685aed69238674009a2c9837254b4
Karsten <mail@irgendeineurl.de>
2022-08-01 22:54:08 +0200

    aufgeräumt

--- Vergleichsoperatoren und der Datentyp bool.py
+++ Vergleichsoperatoren und der Datentyp bool.py
@@ -1,7 +1,7 @@
-print (1 < 5) # (<) kleiner als 
-print(1 <= 5) # (< oder gleich)
-print(1 ==1) # (==) ist gleich
-print( 1 != 2) # (!=) ungleich Operator
+print(1<5) # (<) kleiner als 
+print(1<=5) # (< oder gleich)
+print(1==1) # (==) ist gleich
+print(1!=2) # (!=) ungleich Operator
 
 
 
