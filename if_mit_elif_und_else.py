e4cc7cd2539024cd576ec276278fc6252f85e488
Karsten <mail@irgendeineurl.de>
2022-08-01 23:11:53 +0200

    elif else

---
+++ if_mit_elif_und_else.py
@@ -0,0 +1,11 @@
+age = int(input("Bitte gebe dein Alter ein: "))
+
+if age < 18:
+    print("Du bist jünger als 18")
+elif age == 18:
+    print("Du bist 18")
+else:
+    print("ü18")    
+
+
+print("Ende")
