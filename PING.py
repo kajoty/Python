f1ba4ee2c8a51625ae44a8158441deccf0781589
Karsten Jochens <karsten.jochens@gmail.com>
2023-11-15 23:41:07 +0100

    v 0.4

--- PING.py
+++ PING.py
@@ -1,14 +1,18 @@
 import tkinter as tk
-from tkinter import ttk, scrolledtext, simpledialog, messagebox
+from tkinter import scrolledtext
 import subprocess
-import threading
+import concurrent.futures
 import ipaddress
 import socket
+import threading
 
 class PingApp:
     def __init__(self, root):
         self.root = root
         root.title("IP-Ping-Tool")
+
+        # Variable für die IP-Adresse im CIDR-Format
+        self.ip_range_var = tk.StringVar()
 
         # Label für lokale IP
         self.local_ip_label = tk.Label(root, text="")
@@ -21,19 +25,8 @@ class PingApp:
         label = tk.Label(root, text="Gib einen IP-Adressbereich im CIDR-Format ein (z. B. 192.168.1.0/24):")
         label.pack(pady=10)
 
-        self.ip_range_entry = tk.Entry(root, width=30)
+        self.ip_range_entry = tk.Entry(root, width=30, textvariable=self.ip_range_var)
         self.ip_range_entry.pack(pady=10)
-
-        # Eingabefelder für Portbereich
-        self.port_from_label = tk.Label(root, text="Port von:")
-        self.port_from_label.pack(pady=5)
-        self.port_from_entry = tk.Entry(root, width=10)
-        self.port_from_entry.pack(pady=5)
-
-        self.port_to_label = tk.Label(root, text="Port bis:")
-        self.port_to_label.pack(pady=5)
-        self.port_to_entry = tk.Entry(root, width=10)
-        self.port_to_entry.pack(pady=10)
 
         # Textfeld für die Ausgabe
         self.output_text = scrolledtext.ScrolledText(root, width=50, height=10)
@@ -43,18 +36,16 @@ class PingApp:
         self.wait_label = tk.Label(root, text="")
         self.wait_label.pack(pady=10)
 
-        # Fortschrittsbalken für den Portscan
-        self.progress_bar = ttk.Progressbar(root, orient="horizontal", length=200, mode="determinate")
-
-        self.progress_bar.pack(pady=10)
-
         # Button zum Starten des Ping-Prozesses
-        start_button = tk.Button(root, text="Start", command=self.start_pinging)
-        start_button.pack(pady=10)
+        self.start_button = tk.Button(root, text="Start", command=self.start_pinging)
+        self.start_button.pack(pady=10)
 
-        # Button für Portscan
-        scan_button = tk.Button(root, text="Portscan für ausgewählte IP", command=self.start_port_scan)
-        scan_button.pack(pady=10)
+        # Button zum Stoppen des Ping-Prozesses
+        self.stop_button = tk.Button(root, text="Stop", command=self.stop_pinging, state=tk.DISABLED)
+        self.stop_button.pack(pady=10)
+
+        # Variable für den Status der Suche
+        self.searching = False
 
     def get_local_ip(self):
         try:
@@ -62,51 +53,53 @@ class PingApp:
             s.connect(("8.8.8.8", 80))
             local_ip = s.getsockname()[0]
             s.close()
+
+            # Setze die lokale IP als Vorauswahl im Eingabefeld
             self.local_ip_label.config(text=f"Lokale IP-Adresse des Rechners: {local_ip}")
+            self.ip_range_var.set(f"{local_ip}/24")
+
         except socket.error:
             self.local_ip_label.config(text="Konnte die lokale IP-Adresse nicht abrufen.")
 
-    def run_ping(self, ip_address, results):
+    def run_ping(self, ip_address):
         try:
-            result = subprocess.run(["ping", "-n", "1", ip_address], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, encoding='utf-8', errors='ignore')
+            # Setze das Timeout auf 500 Millisekunden
+            result = subprocess.run(["ping", "-n", "1", "-w", "500", ip_address], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, encoding='utf-8', errors='ignore')
 
             if "Minimum" in result.stdout and ip_address != self.local_ip_label.cget("text").split(":")[1].strip():
-                results.append(ip_address)
+                return f"IP gefunden: {ip_address}"
 
         except subprocess.CalledProcessError:
             pass  # Falls der Ping fehlschlägt, ignorieren wir das
 
     def ping_ip_range(self):
         try:
-            network = ipaddress.IPv4Network(self.ip_range_entry.get(), strict=False)
-            total_addresses = network.num_addresses
-
-            results = []
-            threads = []
+            network = ipaddress.IPv4Network(self.ip_range_var.get(), strict=False)
 
             # Ändere die Nachricht im "Bitte warten"-Label
             self.wait_label.config(text="Bitte warten. Suche läuft.")
 
-            for ip_address in network.hosts():
-                ip_address_str = str(ip_address)
+            self.start_button.config(state=tk.DISABLED)  # Deaktiviere Start-Button
+            self.stop_button.config(state=tk.NORMAL)  # Aktiviere Stop-Button
+            self.searching = True
 
-                thread = threading.Thread(target=self.run_ping, args=(ip_address_str, results))
-                threads.append(thread)
-                thread.start()
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                # Ping für jede IP in einem separaten Thread ausführen
+                results = list(executor.map(self.run_ping, map(str, network.hosts())))
 
-            for thread in threads:
-                thread.join()
+            # Filtere die Ergebnisse und entferne None-Werte
+            results = list(filter(None, results))
 
             # Sortiere die Ergebnisse aufsteigend
-            sorted_results = sorted(results, key=lambda ip: ipaddress.IPv4Address(ip))
+            sorted_results = sorted(results, key=lambda ip: ipaddress.IPv4Address(ip.split(":")[1].strip()))
 
             # Lösche vorherige Ausgabe
             self.output_text.delete(1.0, tk.END)
 
-            for ip_address in sorted_results:
-                self.output_text.insert(tk.END, f"IP gefunden: {ip_address}\n")
+            for result in sorted_results:
+                self.output_text.insert(tk.END, f"{result}\n")
 
-            self.output_text.insert(tk.END, f"\nFertig! Gefundene IPs: {len(sorted_results)} von {total_addresses}\n")
+            self.output_text.insert(tk.END, f"\nFertig! Gefundene IPs: {len(sorted_results)} von {len(network.hosts())}\n")
 
         except ValueError:
             self.output_text.insert(tk.END, "Ungültiges CIDR-Format für den IP-Adressbereich.\n")
@@ -114,59 +107,16 @@ class PingApp:
         finally:
             # Ändere die Nachricht im "Bitte warten"-Label zurück
             self.wait_label.config(text="")
+            # Aktualisiere die Buttons
+            self.start_button.config(state=tk.NORMAL)
+            self.stop_button.config(state=tk.DISABLED)
+            self.searching = False
 
     def start_pinging(self):
         threading.Thread(target=self.ping_ip_range).start()
 
-    def start_port_scan(self):
-        selected_ip = simpledialog.askstring("IP-Auswahl", "Geben Sie die IP-Adresse ein:")
-        if not selected_ip:
-            return
-
-        try:
-            port_from = int(self.port_from_entry.get())
-            port_to = int(self.port_to_entry.get())
-
-            # Führe den Portscan durch
-            self.output_text.delete(1.0, tk.END)
-            threading.Thread(target=self.port_scan, args=(selected_ip, range(port_from, port_to + 1))).start()
-
-        except ValueError:
-            messagebox.showerror("Fehler", "Ungültige Portnummer.")
-
-    def port_scan(self, ip_address, ports):
-        open_ports = []
-
-        # Setze Fortschrittsbalken-Parameter
-        self.progress_bar["maximum"] = len(ports)
-        self.progress_bar["value"] = 0
-
-        for i, port in enumerate(ports):
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(1)
-            result = sock.connect_ex((ip_address, port))
-
-            if result == 0:
-                open_ports.append(port)
-
-            sock.close()
-
-            # Aktualisiere Fortschrittsbalken
-            self.progress_bar["value"] = i + 1
-            self.root.update_idletasks()
-
-        # Zeige das Ergebnis in der Ausgabe an
-        self.output_text.insert(tk.END, f"\nPortscan für IP {ip_address}:\n")
-
-        if open_ports:
-            self.output_text.insert(tk.END, f"Offene Ports: {', '.join(map(str, open_ports))}\n")
-        else:
-            self.output_text.insert(tk.END, "Keine offenen Ports gefunden.\n")
-
-        self.output_text.insert(tk.END, f"\nPortscan für IP {ip_address} abgeschlossen.\n")
-
-        # Infofenster anzeigen
-        messagebox.showinfo("Portscan Ergebnis", f"Portscan abgeschlossen für IP {ip_address}")
+    def stop_pinging(self):
+        self.searching = False
 
 if __name__ == "__main__":
     root = tk.Tk()
